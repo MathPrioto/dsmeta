26b7990cac0dc168e597629fb46a6f23f5bd3afe
MathPrioto <Matheus23nisgoski@gmail.com>
2022-07-14 15:00:14 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButtom from "./components/notification-bottom"
 
 function App() {
   return(
+    <>
     <h1>Olá mundo</h1>
+    <NotificationButtom/>
+      </>
   )
 }
 
---
+++ frontend/src/components/notification-bottom/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/Img/notificaton-icon.svg'
+
+import './styles.css'
+
+function NotificationButtom() {
+    return(
+        <div className="dsmeta-red-btn">
+        <img src={icon} alt="Notificar" />
+      </div>
+    )
+  }
+  
+  export default NotificationButtom
