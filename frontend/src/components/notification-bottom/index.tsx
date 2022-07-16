a082d32c50b2680839b6261ccc3de9d701ab3dad
MathPrioto <Matheus23nisgoski@gmail.com>
2022-07-16 16:44:42 -0300

    Toast

--- frontend/src/components/notification-bottom/index.tsx
+++ frontend/src/components/notification-bottom/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import icon from '../../assets/Img/notificaton-icon.svg';
 import { BASE_URL } from "../../utils/request";
+import { toast } from 'react-toastify';
 
 import './styles.css';
 
@@ -11,9 +12,9 @@ type Props = {
 function handleClick(id :number){
   axios(`${BASE_URL}/sales/${id}/notification` )
       .then(response => {
-        console.log("SUCESSO");
+        toast.info("SMS enviado com sucesso");
       }
-        )
+        );
 }
 
 function NotificationButtom({saleId} : Props) {
